3edb78b9c349b699b99b57aae289218a7bf83a2d
Sofian Mattheus <mattheussofian1@gmail.com>
2022-02-09 16:51:35 +0100

    debut_firebase

---
+++ src/components/Firebase.js
@@ -0,0 +1,17 @@
+const firebaseConfig = {
+
+    apiKey: "AIzaSyBLiWKnUc33ovuCRks4iQLiOFaCQ5ZPUVI",
+  
+    authDomain: "gamers-finder.firebaseapp.com",
+  
+    projectId: "gamers-finder",
+  
+    storageBucket: "gamers-finder.appspot.com",
+  
+    messagingSenderId: "849516353061",
+  
+    appId: "1:849516353061:web:62244ff84afddfbae211cc",
+  
+    measurementId: "G-QPXLN1XJK0"
+  
+  };
--- src/components/TinderCards.js
+++ src/components/TinderCards.js
@@ -5,16 +5,16 @@ function TinderCards() {
     const[people, setPeople] = useState([
         {
             name: 'steve jobs',
-            url : 'https://i.insider.com/5d4dbba92eaed53f8f5b9e95?width=1136&format=jpeg'
+            url : 'https://images.frandroid.com/wp-content/uploads/2021/10/steve-jobs-with-imac-g3-blue.jpeg'
         },
         {
             name: 'elon musk',
-            url: 'https://image.cnbcfm.com/api/v1/image/105318483-1531153006021gettyimages-872508684.jpg?v=1562101726&w=720&h=405'
+            url: 'https://www.presse-citron.net/app/uploads/2019/10/elonmusk-e1611101293534.jpg'
         }
     ]);
 
   return (
-    <div>
+    <div className='card-container'>
         <h1>tinder cards</h1>
 
         {people.map(person => (
